791d28c78720d8e4cf413a7eb7b0114886032ce4
victorsmits <victor.smits@student.vinci.be>
2018-10-15 13:16:22 +0200

    aspect fix

--- gestionnaire contact/js/contacts.js
+++ gestionnaire contact/js/contacts.js
@@ -1,40 +1,45 @@
 var NewContact = {
-    init: function (Lname, Fname) {
-        this.Nom = Lname;
-        this.Prenom = Fname;
-    },
+	init: function (Lname, Fname) {
+		this.Nom = Lname;
+		this.Prenom = Fname;
+	},
 
-    decrire: function () {
-        var description = "Nom : " + this.Nom + ", Prenom : " + this.Prenom;
-        return description;
-    }
+	decrire: function () {
+		var description = "Nom : " + this.Nom + ", Prenom : " + this.Prenom;
+		return description;
+	}
 };
 
 var ContactList = [];
 
 var info = "1 : afficher la liste de contact" + "\n" + "2 : ajouter à la liste de contact" + "\n" + "0 : quitter";
 console.log(info);
-while (true) {
-    var choix = Number(prompt("introduire choix"));
-    if (choix === 1) {
-        console.log("Voici la liste de vos contacts : ");
-        if (ContactList.length === 0) {
-            console.log("vous n'avez pas de contact.")
-        } else {
-            for (var i = 0; i < ContactList.length; i++) {
-                var liste = ContactList[i].decrire();
-                console.log("   " + liste);
-            }
-        }
-    } else if (choix === 2) {
-        var Lname = prompt("Name");
-        var Fname = prompt("Firstname");
-        var Contact = Object.create(NewContact);
-        Contact.init(Lname, Fname);
-        ContactList.push(Contact)
 
-    } else if (choix === 0) {
-        break;
-    }
-    console.log(info);
+while (true) {
+	var choix = Number(prompt("introduire choix"));
+
+	if (choix === 1) {
+		console.log("Voici la liste de vos contacts : ");
+
+		if (ContactList.length === 0) {
+			console.log("vous n'avez pas de contact.");
+		} else {
+			var i;
+			for (i = 0; i < ContactList.length; i++) {
+				var liste = ContactList[i].decrire();
+				console.log("   " + liste);
+			}
+		}
+	} else if (choix === 2) {
+		var Lname = prompt("Name");
+		var Fname = prompt("Firstname");
+		var Contact = Object.create(NewContact);
+		Contact.init(Lname, Fname);
+		ContactList.push(Contact);
+
+	} else if (choix === 0) {
+		break;
+	}
+
+	console.log(info);
 }
