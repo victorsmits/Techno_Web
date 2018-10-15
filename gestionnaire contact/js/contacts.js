66f46658e59ab11b38ae4a39d5c8efd640de7e7d
victorsmits <victor.smits@student.vinci.be>
2018-10-15 11:25:43 +0200

    fix

--- gestionnaire contact/js/contacts.js
+++ gestionnaire contact/js/contacts.js
@@ -10,7 +10,31 @@ var NewContact = {
     }
 };
 
-var Contact1 = Object.create(NewContact);
-Contact1.init("Smits", "Victor")
+var ContactList = [];
 
-console.log(Contact1.decrire());
+var info = "1 : afficher la liste de contact" + "\n" + "2 : ajouter à la liste de contact" + "\n" + "0 : quitter";
+console.log(info);
+while (true) {
+    var choix = Number(prompt("introduire choix"));
+    if (choix === 1) {
+        console.log("Voici la liste de vos contacts : ");
+        if (ContactList.length === 0) {
+            console.log("vous n'avez pas de contact.")
+        } else {
+            for (var i = 0; i < ContactList.length; i++) {
+                var liste = ContactList[i].decrire();
+                console.log("   " + liste);
+            }
+        }
+    } else if (choix === 2) {
+        var Lname = prompt("Name");
+        var Fname = prompt("Firstname");
+        var Contact = Object.create(NewContact);
+        Contact.init(Lname, Fname);
+        ContactList.push(Contact)
+
+    } else if (choix === 0) {
+        break;
+    }
+    console.log(info);
+}
