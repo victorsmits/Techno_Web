b6209968d94b481c1eb9cba667671850bf1f2804
victorsmits <victor.smits@student.vinci.be>
2018-11-20 22:23:14 +0100

    PHP-MVC End

---
+++ PHP-MVC/View/Header.php
@@ -0,0 +1,14 @@
+<!--HTML-->
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="/CSS/PStyle.css">
+</head>
+<div class="navbar">
+    <form method="post">
+        <button type="submit" name="Cart" value="Cart" class="cart"><i class="cart"></i></button>
+    </form>
+</div>
+<body>
