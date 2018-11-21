9fd0423619dbf5b72c2387ac37636e27681d9890
victorsmits <victor.smits@student.vinci.be>
2018-11-21 11:48:18 +0100

    css px->rem

--- PHP-MVC/init.php
+++ PHP-MVC/init.php
@@ -1,6 +1,6 @@
 <?php
 require_once('View/Header.php');
-require_once ('Model/CartModel.php');
+require_once('Model/CartModel.php');
 require_once ('Model/CourseModel.php');
 
 $FormaPHP = new Formation(
