ec37210516ae509843f4d5212e0d31b10fadf617
victorsmits <victor.smits@student.vinci.be>
2018-12-14 15:31:28 +0100

    update jquery

--- JQuery/script.js
+++ JQuery/script.js
@@ -1,52 +1,55 @@
-let state = true;
-$(".test").on({
-	mouseenter: function () {
-		$(this).css({
-			"font-style": "italic",
-			"font-weight": "normal"
-		});
-	},
-	mouseleave: function () {
-		$(this).css({
-			"font-style": "normal",
-			"font-weight": "bold"
-		});
-	},
-	click: function () {
-		if (state) {
-			$(this).css("font-size", "50pt");
-			state = false;
-		} else {
-			$(this).css("font-size", "16pt");
-			state = true;
+$(document).ready(function(){
+	let state = true;
+	$(".test").on({
+		mouseenter: function () {
+			$(this).css({
+				"font-style": "italic",
+				"font-weight": "normal"
+			});
+		},
+		mouseleave: function () {
+			$(this).css({
+				"font-style": "normal",
+				"font-weight": "bold"
+			});
+		},
+		click: function () {
+			if (state) {
+				$(this).css("font-size", "50pt");
+				state = false;
+			} else {
+				$(this).css("font-size", "16pt");
+				state = true;
+			}
 		}
-	}
-});
-$("#B1").click(function () {
-    let rect = $(".rect");
-	let Height = parseInt(rect.css("padding-top"));
-	let add = parseInt($("#px").val());
-	rect.css({
-		"padding-top": Height += add
+	});
+	$("#B1").click(function () {
+		let rect = $(".rect");
+		let Height = parseInt(rect.css("padding-top"));
+		let add = parseInt($("#px").val());
+		rect.css({
+			"padding-top": Height += add
+		});
+	});
+
+	$("#B2").click(function () {
+		let color = $("#color").val();
+		console.log(color);
+		$(".rect").css({
+			"background-color": color
+		});
+	});
+
+	$("#B3").click(function () {
+		$(".rect").hide();
+	});
+
+	$("#B4").click(function () {
+		$(".rect").show();
+	});
+
+	$("#B5").click(function () {
+		$(".rect").removeAttr("style");
 	});
 });
 
-$("button#B2").click(function () {
-	var color = $("input#color").val();
-	console.log(color);
-	$("div.rect").css({
-		"background-color": color
-	});
-});
-
-$("button#B3").click(function () {
-	$("div.rect").hide();
-});
-
-$("button#B4").click(function () {
-	$("div.rect").show();
-});
-
-$("button#B5").click(function () {
-	$("div.rect").removeAttr("style");
-});
